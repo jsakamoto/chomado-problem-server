442bf8342ea395023284f55414716acd84dbdd32
jsakamoto <junichi.jp@gmail.com>
2014-06-21 00:14:09 +0900

    1st commit

---
+++ ChomadProblemServer/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace ChomadProblemServer
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+        }
+    }
+}
---
+++ ChomadProblemServer/ProblemController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ChomadProblemServer
+{
+    [EnableCors("*", "*", "*")]
+    public class ProblemController : ApiController
+    {
+        [HttpPost, Route("answer")]
+        public int Post([FromBody]int[] answers)
+        {
+            var correct = new[] { 4, 3, 1, 2 };
+            return correct
+                .Zip(answers, (c, a) => c == a)
+                .Count(right => right);
+        }
+    }
+}
