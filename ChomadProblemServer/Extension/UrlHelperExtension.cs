18eb833988ff56cefed5f702fbe5abaa407b6a4f
jsakamoto <junichi.jp@gmail.com>
2015-11-26 18:40:02 +0900

    Remove some reference settings to avoiding Mono build failing.

--- ChomadProblemServer/Extension/UrlHelperExtension.cs
+++ ChomadProblemServer/Extension/UrlHelperExtension.cs
@@ -3,8 +3,8 @@ using System.Web;
 
 public static class UrlHelperExtension
 {
-    public static string AppUrl(this HttpRequestBase request)
-    {
-        return request.Url.GetLeftPart(UriPartial.Scheme | UriPartial.Authority);
-    }
+    //public static string AppUrl(this HttpRequestBase request)
+    //{
+    //    return request.Url.GetLeftPart(UriPartial.Scheme | UriPartial.Authority);
+    //}
 }
