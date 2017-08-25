75998fe2d68dfddd0279210a58b43a2a7b639b6d
jsakamoto <junichi.jp@gmail.com>
2017-08-25 22:07:59 +0900

    Port to ASP.NET Core

---
+++ ChomadProblemServer/Controllers/ProblemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace ChomadProblemServer
+{
+    [EnableCors("Any")]
+    public class ProblemController : Controller
+    {
+
+        [HttpGet, Route("/")]
+        public ActionResult Index()
+        {
+            this.ViewBag.SiteBase = this.HttpContext.Request.GetDisplayUrl().TrimEnd('/');
+            return View();
+        }
+
+        [HttpPost, Route("answer")]
+        public int Post([FromBody]int[] answers)
+        {
+            var correct = new[] { 1, 1, 4, 3, 3, 4, 2, 1, 3, 2 };
+            return correct
+                .Zip(answers, (c, a) => c == a)
+                .Count(right => right);
+        }
+    }
+}
