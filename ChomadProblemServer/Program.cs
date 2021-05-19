f2dd35789332fc5bde8eced3839f1e38daa30b4b
jsakamoto <junichi.jp@gmail.com>
2021-05-19 10:05:13 +0900

    Tried to minimize the code

--- ChomadProblemServer/Program.cs
+++ ChomadProblemServer/Program.cs
@@ -1,21 +1,49 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ChomadProblemServer
+WebHost.CreateDefaultBuilder(args)
+.ConfigureServices(services =>
 {
-    public class Program
+    services.AddCors();
+})
+.Configure((webBuilderContext, app) =>
+{
+    if (webBuilderContext.HostingEnvironment.IsDevelopment())
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        app.UseDeveloperExceptionPage();
     }
-}
+    app.UseHttpsRedirection();
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.UseCors();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapMethods("/answer", new[] { "POST", "OPTIONS" }, async context =>
+        {
+            var seedString = context.Request.Query.TryGetValue("seed", out var values) ? values.First() : "1";
+            var seed = int.TryParse(seedString, out var n) ? n : 1;
+
+            var random = new Random(seed);
+            const int numOfOptions = 4;
+            const int numOfQuestions = 10;
+            var correct = Enumerable.Range(0, numOfQuestions).Select(_ => random.Next(0, numOfOptions) + 1).ToArray();
+
+            var answers = await context.Request.ReadFromJsonAsync<int[]>();
+            var correctCount = correct
+                .Zip(answers, (c, a) => c == a)
+                .Count(right => right);
+
+            await context.Response.WriteAsJsonAsync(correctCount);
+        })
+        .RequireCors(option => option.AllowAnyOrigin().AllowAnyHeader().WithMethods("POST"));
+    });
+})
+.Build()
+.Run();
