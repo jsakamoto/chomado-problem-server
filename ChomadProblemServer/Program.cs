b3d4bf2a16357ced6347619768670dfe102d2e62
jsakamoto <junichi.jp@gmail.com>
2022-06-25 21:35:00 +0900

    Refine .NET6 Minimal API implement

--- ChomadProblemServer/Program.cs
+++ ChomadProblemServer/Program.cs
@@ -1,42 +1,27 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();
-builder.Environment.WebRootPath = Path.Combine(builder.Environment.ContentRootPath, "wwwroot");
-builder.Environment.WebRootFileProvider = new PhysicalFileProvider(builder.Environment.WebRootPath);
 
 await using var app = builder.Build();
 if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
-app.UseCors();
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().WithMethods("POST"));
 
-app.MapMethods("/answer", new[] { "POST", "OPTIONS" }, async context =>
+app.MapPost("/answer", ([FromBody] int[] answers, int? seed) =>
 {
-    var seedString = context.Request.Query.TryGetValue("seed", out var values) ? values.First() : "1";
-    var seed = int.TryParse(seedString, out var n) ? n : 1;
-
-    var random = new Random(seed);
+    var random = new Random(seed ?? 1);
     const int numOfOptions = 4;
     const int numOfQuestions = 10;
     var correct = Enumerable.Range(0, numOfQuestions).Select(_ => random.Next(0, numOfOptions) + 1).ToArray();
 
-    var answers = await context.Request.ReadFromJsonAsync<int[]>();
     var correctCount = correct
-    .Zip(answers, (c, a) => c == a)
-    .Count(right => right);
+        .Zip(answers, (c, a) => c == a)
+        .Count(right => right);
 
-    await context.Response.WriteAsJsonAsync(correctCount);
-})
-.RequireCors(option => option.AllowAnyOrigin().AllowAnyHeader().WithMethods("POST"));
+    return correctCount;
+});
 
 await app.RunAsync();
